7a38a15d1f392a1472eb94a1d2fd9b24a8481564
CR Hapuarachchi <112224823+0019-KDU@users.noreply.github.com>
2026-09-24 12:09:46 +0530

    Request S3 bucket shop-images (via Backstage) (#6)

---
+++ resources/s3-bucket/dev/shop-images/main.tf
@@ -0,0 +1,45 @@
+# Requested in Backstage by , owner group:default/platform-admins.
+# Managed by the platform-resources pipeline: edit only via pull request.
+terraform {
+  required_version = ">= 1.10"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "devops94-idp-tfstate-697502032879"
+    key          = "resources/s3-bucket/dev/shop-images/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  default_tags {
+    tags = {
+      project    = "devops94-idp"
+      stack      = "resource-s3-bucket-shop-images"
+      managed-by = "terraform"
+    }
+  }
+}
+
+module "this" {
+  source      = "git::https://github.com/0019-KDU/idp-platform.git//infra/modules/s3-bucket?ref=main"
+  environment = "dev"
+  name        = "shop-images"
+  versioning  = true
+  tags = {
+    owner        = "group:default/platform-admins"
+    component    = "shop-api"
+    requested-by = ""
+  }
+}
+
+output "bucket_name" { value = module.this.bucket_name }
+output "bucket_arn" { value = module.this.bucket_arn }
+output "console_url" { value = module.this.console_url }
